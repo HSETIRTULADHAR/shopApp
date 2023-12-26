977ad347ee92be1b0cde3cb726670f37e50e7112
HSETIRTULADHAR <52373550+HSETIRTULADHAR@users.noreply.github.com>
2023-12-26 18:42:09 +0545

    Update SingleProduct.jsx

--- src/shop/SingleProduct.jsx
+++ src/shop/SingleProduct.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import PageHeader from '../components/PageHeader';
-
+import data from '../products.json';
 
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
